fa5e106c914b210666db31140228cd9a8630b509
Francesco Piantini <francesco.piantini@thalesgroup.com>
2021-05-13 09:49:55 +0200

    complete reworking: moving to ECS modelization 2 (STILL NOT WORKING)

---
+++ src/systems/entity_render.rs
@@ -0,0 +1,10 @@
+use crate::prelude::*;
+
+#[system]
+#[read_component(Point)]
+#[read_component(Render)]
+pub fn entity_render(
+    ecs: SubWorld,
+    #[resource] camera: &Camera
+)
+{}
--- src/systems/player_input.rs
+++ src/systems/player_input.rs
@@ -1,6 +1,46 @@
 use crate::prelude::*;
 
+
+// Legion systems are quite complicated internally. It uses Procedural Macros (commonly
+// referred to as "proc macros") to save you from writing a lot of repetitive boilerplate
+// code for each system.
+// #[system] is a procedural macro
+
+// The #[system] line annotates the player_input function with a procedural macro
+// named system . This macro transforms your function name into player_input_system ,
+// and wraps it with all of the extra code Legion requires to construct a system.
+// #[write_component] requests writable access to a component type
+// #[read_component] requests read-only access to a component type
+// #[resource] requests access to types you stored in Legion’s Resource handler
+// (see main.rs)
 #[system]
-pub fn player_input() {
-    
+#[write_component(Point)]
+#[read_component(Player)]
+pub fn player_input(
+    ecs: &mut SubWorld,
+    #[resource] map: &Map,
+    #[resource] key: &Option<VirtualKeyCode>,
+    #[resource] camera: &mut Camera,
+)
+{
+    if let Some(key) = key {
+        let delta = match key {
+            VirtualKeyCode::Left => Point::new(-1, 0),
+            VirtualKeyCode::Right => Point::new(1, 0),
+            VirtualKeyCode::Up => Point::new(0, -1),
+            VirtualKeyCode::Down => Point::new(0, 1),
+            _ => Point::new(0, 0),
+        };
+        if delta.x != 0 || delta.y != 0 {
+            let mut players = <&mut Point>::query()
+                .filter(components::Player());
+            players.iter_mut(ecs).for_each(|pos| {
+                let destination = *pos + delta;
+                if map.can_enter_tile(destination) {
+                    *pos = destination;
+                    camera.on_player_move(destination);
+                }
+            });
+        }
+    }
 }
